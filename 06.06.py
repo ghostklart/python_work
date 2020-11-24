a279f880a8474338d49cbad4732ac4e93d1d00ff
ghostklart <ghostklart@gmail.com>
2020-11-24 13:56:32 +0000

    new files from study

--- 06.06.py
+++ 06.06.py
@@ -1,23 +1,26 @@
-#6.5
-#rivers
-rivers = {
-    'nile': 'egypt',
-    'amazon': 'america',
-    'moskva': 'russia'
+#6.7
+#favorite languages
+favorite_langauges = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
 }
-#print cycle
-for river, country in rivers.items():
-    message = f"The {river.title()} runs through {country.title()}."
-    print(message)
-#print cycle only river names
-mesg = "\nThe rivers in the list are:"
-print(mesg)
-for river in rivers.keys():
-    smesg = f"{river.title()}"
-    print(smesg)
-#print country names
-mesg = "\nThe countries in the list are:"
-print(mesg)
-for country in rivers.values():
-    smesg = f"{country.title()}"
-    print(smesg)
+#people to pass the test
+ppl_tst = [
+    'sergey',
+    'sarah',
+    'phil',
+    'damir',
+    'andrey'
+]
+#cycle to check
+for ppl in sorted(ppl_tst):
+    if ppl in favorite_langauges.keys():
+        #wmsg for warning message
+        wmsg = f"{ppl.title()}, thank you for participating!"
+        print(wmsg)
+    else:
+        #smsg for status message
+        smsg = f"Dear {ppl.title()}, please take our poll!"
+        print(smsg) 
--- 06.07.py
+++ 06.07.py
@@ -1,26 +1,39 @@
 #6.7
-#favorite languages
-favorite_langauges = {
-    'jen': 'python',
-    'sarah': 'c',
-    'edward': 'ruby',
-    'phil': 'python'
+#multi dictionary
+person_0 = {
+    'first_name': 'alexander',
+    'last_name': 'hamilton',
+    'city': 'london',
+    'age': 16
 }
-#people to pass the test
-ppl_tst = [
-    'sergey',
-    'sarah',
-    'phil',
-    'damir',
-    'andrey'
-]
-#cycle to check
-for ppl in ppl_tst:
-    if ppl in favorite_langauges.keys():
-        #wmsg for warning message
-        wmsg = f"{ppl.title()}, thank you for participating!"
-        print(wmsg)
-    else:
-        #smsg for status message
-        smsg = f"Dear {ppl.title()}, please take our poll!"
-        print(smsg)
+
+person_1 = {
+	'first_name': 'aaron',
+	'last_name': 'burr',
+	'city': 'new york',
+	'age': 21
+}
+
+person_2 = {
+	'first_name': 'luke',
+	'last_name': 'skywalker',
+	'city': 'tatooine',
+	'age': 18
+}
+
+people = (
+	person_0,
+	person_1,
+	person_2
+	)
+
+#print(people)
+#for person in people:
+#	print(person)
+
+for person in people:
+	full_name = person['first_name'] + " " + person['last_name']
+	age = person['age']
+	location = person['city']
+	message = f"{full_name.title()} at the age of {age} lives in {location.title()}."
+	print(message)
